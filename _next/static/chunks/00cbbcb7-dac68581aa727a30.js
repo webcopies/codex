2efa5130b75ec97c056ec4a43a5c41f3a0542005
Web-Copies <162612386+Web-Copies@users.noreply.github.com>
2024-03-08 14:52:04 +0530

    Update 00cbbcb7-dac68581aa727a30.js

--- _next/static/chunks/00cbbcb7-dac68581aa727a30.js
+++ _next/static/chunks/00cbbcb7-dac68581aa727a30.js
@@ -1,1 +1,28 @@
+"use strict";
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([[447], {
+    4606: function(module, exports, __webpack_require__) {
+        __webpack_require__.d(exports, {
+            KpZ: function() {
+                return createIcon
+            }
+        });
+        var __webpack_module__1172__ = __webpack_require__(1172);
+
+        function createIcon(pathData) {
+            return (0, __webpack_module__1172__.w_)({
+                tag: "svg",
+                attr: {
+                    fill: "currentColor",
+                    viewBox: "0 0 16 16"
+                },
+                child: [{
+                    tag: "path",
+                    attr: {
+                        d: pathData
+                    }
+                }]
+            })(module);
+        }
+    }
+}]);
 
